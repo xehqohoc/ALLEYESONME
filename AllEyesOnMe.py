46c8e4a9f9bc9e42a7b643ee40eefe1f8e798da8
xehqohoc <pajinsorry@gmail.com>
2024-09-07 12:00:16 -0500

    Create AllEyesOnMe.py

---
+++ AllEyesOnMe.py
@@ -0,0 +1,14 @@
+print("    ___    __________          _____          ")
+print("   /   |  / / / ____/_  _____ / ___/          ")
+print("  / /| | / / / __/ / / / / _ \\__ \              ")
+print("/ ___ |/ / / /___/ /_/ /  __/__/ /          ")
+print("/_/  |_/_/_/_____/\__, /\___/____/     ")
+print("                 /____/")
+
+print("------------------------------------------------------------")
+print("           <1> FACEBOOK ")
+print("           <2> INSTAGRAM (NO DISPONIBLE)")
+print("-----------------------------------------------------------")
+options = int(input("seleccione una opcion:"))
+if options == 1:
+	print("URL HOSTER: sofiavalevip.webcindario.com")
